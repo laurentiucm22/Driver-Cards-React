79afbd6281d0820e3b2aa5372adf042e87d7a594
Laurentiu CM <laurentiu.costachescu22@gmail.com>
2022-08-15 03:48:39 +0300

    Add fixed the search logic

--- drivers-cards/src/pages/DriversPage.js
+++ drivers-cards/src/pages/DriversPage.js
@@ -1,26 +1,55 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import DriverCard from "../components/DriverCard";
 import classes from "./DriversPage.module.css";
 import { BsSearch } from "react-icons/bs";
 
 const DriversPage = ({ mockData }) => {
   const [driversList, setDriversList] = useState(mockData);
-  const [searchDriver, setSearchDriver] = useState("");
+  const inputRef = useRef();
 
   const sortingDriverList = () => {
-    return driversList.sort((a, b) => {
+    return [...driversList].sort((a, b) => {
       return b.points - a.points;
     });
   };
 
   const incrementingDriverPoints = (number) => {
     let findDriverArr = [...mockData];
-    console.log(findDriverArr);
     let findDriver = findDriverArr.find((el) => {
       return el.number === number;
     });
+    // .points++;
+    // console.log(findDriver);
     findDriver.points = findDriver.points + 1;
     setDriversList(findDriverArr);
+  };
+
+  const handleSeachDrivers = (event) => {
+    const searchValue = event.target.value.toLowerCase().trim();
+    const splitedSearchValue = searchValue.split(" ");
+
+    const result = [...mockData].filter((driver) => {
+      const driverFirstName = driver.firstName.toLowerCase();
+      const driverLastName = driver.lastName.toLowerCase();
+      return (
+        // prettier-ignore
+        (driverFirstName.includes(searchValue) || driverLastName.includes(searchValue)) || (splitedSearchValue.includes(driverFirstName) || splitedSearchValue.includes(driverLastName))
+      );
+    });
+
+    if (result.length > 0) {
+      setDriversList([...result]);
+    }
+    if (result.length === 0) {
+      setDriversList([]);
+    }
+    if (!searchValue) {
+      setDriversList([...mockData]);
+    }
+  };
+
+  const onStartSearch = () => {
+    inputRef.current.focus();
   };
 
   return (
@@ -30,11 +59,14 @@ const DriversPage = ({ mockData }) => {
           type="search"
           placeholder="Search driver"
           className={classes["driver_search-input"]}
-          value={searchDriver}
-          onChange={(event) => setSearchDriver(event.target.value)}
+          onChange={handleSeachDrivers}
+          ref={inputRef}
         />
-        <BsSearch className={classes["search_icon"]} />
+        <BsSearch className={classes["search_icon"]} onClick={onStartSearch} />
       </div>
+      {driversList.length === 0 && (
+        <span className={classes["search_no-result"]}>No result found...</span>
+      )}
       {sortingDriverList().map(
         (
           { number, points, firstName, lastName, country, team, hex, image },
@@ -42,8 +74,6 @@ const DriversPage = ({ mockData }) => {
         ) => {
           return (
             <DriverCard
-              mockData={mockData}
-              handleSortData={sortingDriverList}
               incrementingDriverPoints={incrementingDriverPoints}
               data={driversList}
               number={number}
